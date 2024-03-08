2060ae08079406fb37aacc60ebd83abb4cf1ea0b
Yang <bboylevi89@gmail.com>
2024-03-08 11:03:42 +0000

    Update MCVI codes in Julia

---
+++ src/AlphaVectorFSC.jl
@@ -0,0 +1,81 @@
+
+mutable struct FscNode
+    _state_particles::Vector{Any}
+    _Q_action::Dict{Any,Float64}
+    _R_action::Dict{Any,Float64} # expected instant reward 
+    _V_a_o_n::Dict{Any, Dict{Int64, Dict{Int64, Float64}}}
+    _V_node_s::Dict{Any, Float64}
+    _V_node_s_count::Dict{Any, Int64}
+    _V_node::Float64
+    _best_action_update::Dict{Any,Bool}
+end
+
+
+mutable struct FSC
+    _eta::Vector{Dict{Pair{Any,Int64},Int64}}
+    _nodes::Vector{FscNode}
+    _action_space
+    _obs_space
+end
+
+function InitFscNode(action_space, obs_space)
+    init_particles = []
+    # --- init for actions ---
+    init_Q_action = Dict{Any,Float64}()
+    init_R_action = Dict{Any,Float64}()
+    init_V_a_o_n = Dict{Any,Dict{Int64,Dict{Int64,Float64}}}() 
+    init_best_action_update = Dict{Any, Bool}()
+    for a in action_space
+        init_Q_action[a] = 0.0
+        init_R_action[a] = 0.0
+        init_V_a_o_n[a] = Dict{Int64, Dict{Int64, Float64}}()
+        for o in obs_space
+            init_V_a_o_n[a][o] =  Dict{Int64, Float64}()
+        end
+        init_best_action_update[a] = false 
+    end
+    init_V_node_s = Dict{Any, Float64}()
+    init_V_node_s_count = Dict{Any, Int64}()
+    init_V_node = 0.0
+    return FscNode(init_particles,
+                    init_Q_action,
+                    init_R_action,
+                    init_V_a_o_n,
+                    init_V_node_s,
+                    init_V_node_s_count,
+                    init_V_node,
+                    init_best_action_update)
+end
+
+function CreatNode(b, action_space, obs_space)
+    node = InitFscNode(action_space, obs_space)
+    node._state_particles = b
+    return node
+end
+
+
+function InitFSC(max_node_size::Int64, action_space, obs_space)
+    init_eta = Vector{Dict{Pair{Any,Int64},Int64}}(undef, max_node_size)
+    for i in range(1, stop=max_node_size)
+        init_eta[i] = Dict{Pair{Any,Int64},Int64}()
+    end
+    init_nodes = Vector{FscNode}()
+    return FSC(init_eta,
+                init_nodes,
+                action_space,
+                obs_space)
+end
+
+function GetBestAction(n::FscNode)
+    Q_max = typemin(Float64)
+    best_a = rand(keys(n._Q_action))
+    for (key, value) in n._Q_action
+        if value > Q_max
+            Q_max = value
+            best_a = key
+        end
+    end
+
+    return best_a
+end
+
---
+++ src/Planner.jl
@@ -0,0 +1,334 @@
+include("./AlphaVectorFSC.jl")
+using POMDPModels
+using POMDPs
+using POMDPModelTools
+using Random
+
+function SimulateTrajectory(nI::Int64, fsc::FSC, s::Any,  L::Int64, pomdp) 
+    gamma = discount(pomdp)
+    
+    V_n_s = 0.0
+    nI_current = nI
+    for step in 0:L
+        if (isterminal(pomdp, s))
+			break
+		end
+        a = rand(actions(pomdp))
+        if nI_current != -1
+            a = GetBestAction(fsc._nodes[nI_current])
+            sp, o, r = @gen(:sp, :o, :r)(pomdp, s, a)
+            if haskey(fsc._eta[nI_current], Pair(a, o))
+                nI_current = fsc._eta[nI_current][Pair(a, o)]
+            else
+                nI_current = -1 
+            end 
+        else 
+            sp, o, r = @gen(:sp, :o, :r)(pomdp, s, a)
+        end
+        V_n_s += (gamma^step)*r
+        s = sp
+    end
+    return V_n_s
+end
+
+
+function FindMaxValueNode(n:: FscNode, fsc::FSC, a, o)
+    max_V = typemin(Float64)
+    max_nI = 1
+    for nI in 1:length(fsc._nodes)
+        V_temp = n._V_a_o_n[a][o][nI]
+        if V_temp > max_V
+            max_V = V_temp
+            max_nI = nI
+        end
+    end
+    return max_V, max_nI
+end
+
+function FindRLower(pomdp, b0, action_space)
+	action_min_r = Dict{Any, Float64}()
+	for a in action_space
+		min_r = typemax(Float64)
+		for i in 1:100
+			s = rand(b0)
+			step = 0
+			while (discount(pomdp)^step) > 0.01 && isterminal(pomdp, s) == false
+				sp, o, r = @gen(:sp, :o, :r)(pomdp, s, a)
+				s = sp
+				if r < min_r
+					action_min_r[a] = r
+					min_r = r
+				end
+				step += 1
+			end
+		end
+	end
+
+	max_min_r = typemin(Float64)
+	for a in action_space
+		if (action_min_r[a] > max_min_r)
+			max_min_r = action_min_r[a]
+		end
+	end
+
+	return max_min_r / (1 - discount(pomdp))
+end
+
+# function BackUp(b, fsc::FSC, L::Int64, nb_sample::Int64, pomdp)
+function BackUp(nI_new::Int64, fsc::FSC, RL::Float64, L::Int64, nb_sample::Int64, pomdp, action_space, obs_space)
+    #  a new node (alpha-vector in MCVI)
+    gamma = discount(pomdp)
+    V_nI_new = fsc._nodes[nI_new]._V_node
+    println("nI_new $nI_new, V $V_nI_new")
+    for a in action_space
+        fsc._nodes[nI_new]._R_action[a] = 0.0
+        fsc._nodes[nI_new]._Q_action[a] = 0.0 #  Do we need init Q?
+        for o in obs_space
+            for nI in 1:length(fsc._nodes)
+                fsc._nodes[nI_new]._V_a_o_n[a][o][nI] = 0.0
+            end
+        end
+    end
+
+    for a in action_space
+        for i in 1:nb_sample
+            s = rand(fsc._nodes[nI_new]._state_particles)
+            sp, o, r = @gen(:sp, :o, :r)(pomdp, s, a)
+            fsc._nodes[nI_new]._R_action[a] += r
+            for nI in 1:length(fsc._nodes)
+                V_nI_sp = SimulateTrajectory(nI, fsc, sp, L, pomdp) 
+                fsc._nodes[nI_new]._V_a_o_n[a][o][nI] += V_nI_sp
+            end 
+        end
+
+        for o in obs_space
+            V_a_o, nI_a_o = FindMaxValueNode(fsc._nodes[nI_new], fsc, a, o)
+            # fsc._eta[nI_new][a][o] = nI_a_o
+            fsc._eta[nI_new][Pair(a, o)] = nI_a_o
+            fsc._nodes[nI_new]._Q_action[a] += discount(pomdp)*V_a_o
+        end
+        fsc._nodes[nI_new]._Q_action[a] += fsc._nodes[nI_new]._R_action[a]
+        fsc._nodes[nI_new]._Q_action[a] /= nb_sample
+    end
+
+    best_a = GetBestAction(fsc._nodes[nI_new])
+    fsc._nodes[nI_new]._V_node = fsc._nodes[nI_new]._Q_action[best_a]
+    # return nI_new
+end
+
+# Update Beliefs with MC sampling
+function BeliefUpdate(b, a, nb_sim::Int64, pomdp)
+    next_beliefs = Dict{Any, Any}() # a map from observations to beliefs
+    for i in 1:nb_sim
+        s = rand(b)
+        sp, o, r = @gen(:sp, :o, :r)(pomdp, s, a)
+        if haskey(next_beliefs, o)
+            push!(next_beliefs[o], sp)
+        else
+            next_beliefs[o] = [sp]
+        end
+    end
+
+    return next_beliefs 
+end
+
+# Expand beliefs
+function ExpandBeliefs(fsc::FSC, 
+                        nI::Int64, 
+                        s, 
+                        nb_sim::Int64,
+                        current_step::Int64, 
+                        L::Int64,
+                        pomdp, 
+                        action_space, 
+                        obs_space, 
+                        belief_node_list::Vector{Int64})
+
+    # Expand beliefs from root
+    if current_step < L && !isterminal(pomdp, s)
+        
+        if !(nI in belief_node_list)
+            push!(belief_node_list, nI)
+            # println("add node $nI")
+        end 
+
+        a = GetBestAction(fsc._nodes[nI])
+        # if a != fsc._nodes[nI]._best_action && 
+            # fsc._nodes[nI]._best_action = a
+        if fsc._nodes[nI]._best_action_update[a] == false 
+            println("adding new beliefs from node $nI with action $a")
+            new_beliefs = BeliefUpdate(fsc._nodes[nI]._state_particles, a, nb_sim, pomdp)
+            for (o, b_new) in new_beliefs
+                n_new = CreatNode(b_new, action_space, obs_space)
+                push!(fsc._nodes, n_new)
+                nI_new = length(fsc._nodes)
+                push!(belief_node_list, nI_new)
+            end
+            fsc._nodes[nI]._best_action_update[a] = true
+        else
+            sp, o, r = @gen(:sp, :o, :r)(pomdp, s, a)
+            n_nextI = fsc._eta[nI][Pair(a, o)]
+            # println("go one more step to node $n_nextI")
+            ExpandBeliefs(fsc, n_nextI, sp, nb_sim, current_step + 1, L, pomdp, action_space, obs_space, belief_node_list)
+        end
+    end
+end
+
+
+function ExpandBeliefsWithBestActions(fsc::FSC) 
+    nI = 1
+    open_list = [nI]
+    result_list = [nI]
+    while length(open_list) > 0
+        nI = last(open_list)
+        deleteat!(open_list, length(open_list))
+        a = GetBestAction(fsc._nodes[nI])
+        for (k,v) in fsc._eta[nI]
+            if (k[1] == a) && !(v in result_list)
+                push!(open_list, v)
+                push!(result_list, v)  
+            end
+        end
+    end
+
+    return result_list
+end
+
+
+function MCVIPlanning(b0, 
+                        fsc::FSC, 
+                        pomdp, 
+                        RL::Float64,
+                        L::Int64, 
+                        nb_sample::Int64, 
+                        epsilon::Float64, 
+                        nb_iter::Int64)
+
+    action_space = actions(pomdp)
+    obs_space = observations(pomdp)
+    node = CreatNode(b0, action_space, obs_space)
+    push!(fsc._nodes, node)
+    nI_start = length(fsc._nodes)
+    belief_node_list = [nI_start]
+    # backup_list = [nI_start]
+    # The solver will stop when bounds (Upper - Lower) < epsilon
+    # Expand Belief first?
+    # Each belief will create a new alpha vector (a new FSC node)
+    # Currently just do fixed iteration
+
+    for i in 1:nb_iter
+        # Add new alpha vector node for each b in belief set
+        V_root = typemin(Float64)
+        # while length(belief_node_list) != 0
+
+        println("!! Belief Expand Process !!")
+        belief_node_list = Vector{Int64}()
+        # Expand belief set
+        ExpandBeliefs(fsc, nI_start, rand(b0), nb_sample, 0, L, pomdp, action_space, obs_space, belief_node_list)
+        reverse!(belief_node_list)
+
+        println("BackUp Process")
+        while abs(V_root - fsc._nodes[nI_start]._V_node) > epsilon
+            V_root = fsc._nodes[nI_start]._V_node
+            # nI = pop!(belief_node_list)
+            for nI in 1:length(fsc._nodes)
+                # nI_input = length(fsc._nodes) + 1 - nI
+                BackUp(nI, fsc, RL, L, nb_sample, pomdp, action_space, obs_space)
+            end
+
+        end
+
+        # backup_list = ExpandBeliefsWithBestActions(fsc)
+    end
+
+    return fsc
+end
+
+
+function SimulationWithFSC(b0, pomdp, fsc::FSC, steps::Int64)
+	s = rand(b0)
+	sum_r = 0.0
+	nI = 1
+	for i in 1:steps
+		if (isterminal(pomdp, s))
+			break
+		end
+
+		println("---------")
+		println("step: ", i)
+		println("state:", s)
+		a = GetBestAction(fsc._nodes[nI])
+		println("perform action:", a)
+		sp, o, r = @gen(:sp, :o, :r)(pomdp, s, a)
+		s = sp
+		sum_r += (discount(pomdp)^i) * r
+		println("recieve obs:", o)
+		println("nI:", nI)
+		println("nI value:", fsc._nodes[nI]._V_node)
+		nI = fsc._eta[nI][Pair(a, o)]
+		println("reward:", r)
+	end
+
+	println("sum_r:", sum_r)
+end
+
+
+function EvaluationWithSimulationFSC(b0, pomdp, fsc::FSC, discount::Float64, nb_sim::Int64)
+
+	sum_r = 0.0
+	sum_r_valid = 0.0
+	sum_unvalid_search = 0
+
+
+	# set a default action
+	a_safe = GetBestAction(fsc._nodes[1]) # should be a safe action, or a greedy action 
+
+	for sim_i in 1:nb_sim
+		step = 0
+		sum_r_sim_i = 0.0
+		s = rand(b0)
+		nI = 1
+		bool_random_pi = false
+		bool_sim_i_invalid = false
+
+		while (discount^step) > 0.01 && isterminal(pomdp, s) == false
+			if bool_random_pi == true && bool_sim_i_invalid == false
+				bool_sim_i_invalid = true
+				sum_unvalid_search += 1
+			end
+
+			if nI == -1
+				bool_random_pi = true
+			# elseif fsc._nodes[nI]._visits_node == 0
+			# 	bool_random_pi = true
+			end
+
+			if bool_random_pi
+				# a = rand(fsc._action_space)
+				a = a_safe
+			else
+				a = GetBestAction(fsc._nodes[nI])
+			end
+
+			sp, o, r = @gen(:sp, :o, :r)(pomdp, s, a)
+			s = sp
+			sum_r_sim_i += (discount^step) * r
+
+			if haskey(fsc._eta[nI], Pair(a, o))
+				nI = fsc._eta[nI][Pair(a, o)]
+			else
+				bool_random_pi = true
+			end
+			step += 1
+		end
+		sum_r += sum_r_sim_i
+		if bool_sim_i_invalid == false
+			sum_r_valid += sum_r_sim_i
+		end
+	end
+
+	println("avg sum:", sum_r / nb_sim)
+	println("avg sum valid search:", sum_r_valid / (nb_sim - sum_unvalid_search))
+	println("unvalid search:", sum_unvalid_search / nb_sim)
+end
+
